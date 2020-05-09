18fc926b0030575f38d7bda97a10f5a644fba386
sine/cosine/tangent <zfshmn@gmail.com>
2020-05-09 16:01:34 -0700

    walletcore jar + some boilerplate + related dependency wrangling

--- app/src/main/java/com/pylons/loud/activities/MainActivity.kt
+++ app/src/main/java/com/pylons/loud/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pylons.loud.R
+import com.pylons.loud.utils.CoreController
 import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         init()
     }
 
     private fun init() {
+
         val sharedPref = getSharedPreferences(getString(R.string.preference_file_account), Context.MODE_PRIVATE)
         sharedPref.all.forEach {
             Log.info(it.toString())
         }
+
+        CoreController.bootstrapCore() // should actually call setUserData first
 
         val currentUsername = sharedPref.getString(getString(R.string.key_current_account), "");
         if (currentUsername.equals("")) {
